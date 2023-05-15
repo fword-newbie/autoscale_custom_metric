57646327ac67b40fe979f0a850d8b905bea55cd7
root <root@k8s-master.maas>
2023-05-15 10:50:39 +0800

    plus_pu&k6

---
+++ easyone/docker_k6/k6.js
@@ -0,0 +1,43 @@
+import http from "k6/http";
+import { check, sleep } from "k6";
+
+export const options = {
+  thresholds: {
+    http_req_duration: ["p(99) < 3000"],
+  },
+  stages: [
+    { duration: "30s", target: 15 },
+    { duration: "1m", target: 15 },
+    { duration: "20s", target: 0 },
+  ],
+};
+
+export default function () {
+  let res = http.get("http://puyuan-s:5000/api/shock");
+  // let res = http.get("http://httpcpuwaste-n:31112/adam");
+  check(res, { "status was 200": (r) => r.status == 200 });
+  sleep(1);
+}
+
+// export default function () {
+
+//   var url = 'http://puyuan-s:5000/api/shock';
+  
+//   const params = {
+//   headers: {
+//     'Content-Type':'application/json',
+//     'Accept':'application/json' 
+//   },
+//   timeout: 200,
+//   };
+  
+//   let data = { name: 'jojo' };
+
+//   let res = http.post(url,data,params);
+//   console.log(res.body);
+  
+//   // Verify response 
+//   check(res, {
+//   'status is 200': (r) => r.status === 200
+//   });
+//   }
